c471bda4012dcf1d2b0beaba18b73fd7e3423ff0
Fernando Borretti <fernando@borretti.me>
2022-04-17 22:17:04 +0000

    Add highlight.js and a highlighting mode for Austral

---
+++ assets/austral-highlight.js
@@ -0,0 +1,59 @@
+function australMode(hljs) {
+    return {
+        name: "Austral",
+        case_insensitive: false,
+        keywords: {
+            keyword: [
+                "and",
+                "or",
+                "not",
+                "module",
+                "body",
+                "is",
+                "end",
+                "import",
+                "as",
+                "constant",
+                "type",
+                "record",
+                "union",
+                "function",
+                "typeclass",
+                "method",
+                "instance",
+                "generic",
+                "if",
+                "then",
+                "else",
+                "return",
+                "case",
+                "of",
+                "when",
+                "let",
+                "while",
+                "for",
+                "from",
+                "to",
+                "do",
+                "skip",
+                "Free",
+                "Linear",
+                "Type",
+                "Region",
+                "pragma",
+                "sizeof"
+            ],
+            literal: [
+                "nil",
+                "false",
+                "true"
+            ]
+        },
+        contains: [
+            hljs.COMMENT('--', '$')
+        ],
+    };
+};
+
+hljs.registerLanguage("austral", australMode);
+hljs.highlightAll();
